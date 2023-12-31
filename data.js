0802d3664d41da6d3183a91bc18ddd5107468c85
Zunepy <154877247+Zunepy@users.noreply.github.com>
2023-12-31 13:53:26 +0530

    Music Playlist

---
+++ data.js
@@ -0,0 +1,68 @@
+let songs = [
+    {
+        name: 'Mulaqat',
+        path: 'Music/Mulaqat.mp3',
+        artist: 'Prateek Kuhad',
+        cover: 'images/Mulaqat.png'
+    },
+    {
+        name: 'Tumhe kitna pyar karte',
+        path: 'Music/Tumhe Kitna Pyar Karte.mp3',
+        artist: 'Arijit Singh',
+        cover: 'Images/Tumhe kitna pyar karte.png'
+    },
+    {
+        name: 'Kashmir',
+        path: 'Music/Kashmir.mp3',
+        artist: 'Shreya Ghoshal',
+        cover: 'Images/Kashmir.png'
+    },
+    {
+        name: 'Mere Bina',
+        path: 'Music/Mere Bina.mp3',
+        artist: "Nikhil D'Souza",
+        cover: 'Images/Mere Bina.png'
+    },
+    {
+        name: 'Ghoomey',
+        path: 'Music/Ghoomey.mp3',
+        artist: 'Jubin Nautiyal',
+        cover: 'Images/Ghoomey.png'
+    },
+    {
+        name: 'Itni Si Baat',
+        path: 'Music/Itni Si Baat.mp3',
+        artist: 'Sonu Nigam',
+        cover: 'Images/Itni Si Baat.png'
+    },
+    {
+        name: 'Tum Se Milke',
+        path: 'Music/Tum Se Milke.mp3',
+        artist: 'Mohit Chauhan',
+        cover: 'Images/Tum Se Milke.png'
+    },
+    {
+        name: 'Saudebaazi',
+        path: 'Music/Saudebaazi.mp3',
+        artist: 'Javed Ali',
+        cover: 'Images/Saudebaazi.png'
+    },
+    {
+        name: 'Sukoon Mila',
+        path: 'Music/Sukoon Mila.mp3',
+        artist: 'Arijit Singh',
+        cover: 'Images/Sukoon Mila.png'
+    },
+    {
+        name: 'Meet',
+        path: 'Music/Meet.mp3',
+        artist: 'Arijit Singh',
+        cover: 'Images/Meet.png'
+    },
+    {
+        name: 'Hum Dono',
+        path: 'Music/Hum Dono.mp3',
+        artist: 'Prateek Kuhad',
+        cover: 'Images/Hum Dono.png'
+    },
+]
